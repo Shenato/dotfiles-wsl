9f9b9c72acb11580f6a6343e8125db8246b24038
Omar <urreaped@gmail.com>
2024-10-13 13:33:56 +0200

    Update install_wsl.sh

--- install_wsl.sh
+++ install_wsl.sh
@@ -4,3 +4,6 @@ apt install zsh
 # installing zgen from https://github.com/tarjoilija/zgen
 git clone https://github.com/tarjoilija/zgen.git "${HOME}/.zgen"
 source "${HOME}/.zgen/zgen.zsh"
+
+# install nvm from https://github.com/nvm-sh/nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
