c2a84a34ffdcc67580b246daf308db7147dc20ee
Omar <urreaped@gmail.com>
2024-10-13 13:31:42 +0200

    Create install_wsl.sh

---
+++ install_wsl.sh
@@ -0,0 +1,6 @@
+# installing zsh from https://github.com/ohmyzsh/ohmyzsh/wiki/Installing-ZSH
+apt install zsh
+
+# installing zgen from https://github.com/tarjoilija/zgen
+git clone https://github.com/tarjoilija/zgen.git "${HOME}/.zgen"
+source "${HOME}/.zgen/zgen.zsh"
